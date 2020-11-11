f8a453afbea033254e1e13a07d9ce42047ee2fc7
lucas-ye <yeweijiang10@gmail.com>
2020-11-11 20:46:02 -0300

    terminou

--- guerra.cpp
+++ guerra.cpp
@@ -29,13 +29,16 @@ void init(Pilha *jogadorA, Pilha *jogadorB){
 }
 
 void mostraRodada(cartaBaralho a, cartaBaralho b) {
-	cout << a.valorCarta() << " " << a.valorNaipe() << endl;
-	cout << b.valorCarta() << " " << b.valorNaipe() << endl;
+	string naipe[] = {"Espadas", "Ouros", "Paus", "Copas"};
+	string c[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	cout <<"\t"<< c[a.valorCarta() - 1] <<"\t"<< naipe[a.valorNaipe()] << endl;
+	cout <<"\t"<< c[b.valorCarta() - 1] <<"\t"<< naipe[b.valorNaipe()] << endl;
 }
 
 void comparaCartas(cartaBaralho a, cartaBaralho b, Pilha *areaCombate, Pilha *prisioneiroA, Pilha *prisioneiroB) {
 	if (a.valorCarta() != b.valorCarta()) {
-		if (a.valorCarta() > b.valorCarta()) {
+		cartaBaralho carta;
+		if (a.valorCarta() > b.valorCarta()){
 			while (!areaCombate->vazia()){
 				prisioneiroA->inserir(areaCombate->topo());
 				areaCombate->remover();
@@ -46,27 +49,48 @@ void comparaCartas(cartaBaralho a, cartaBaralho b, Pilha *areaCombate, Pilha *pr
 				areaCombate->remover();
 			}
 		}
-		cout << "-------------------------------------" << endl;
-	}else{
-		areaCombate->imprime();
+		cout << "--------------------------------------" << endl;
+	}
+}
+void pegaDoPrisioneiro(Pilha *jogadorA, Pilha *jogadorB, Pilha *prisioneiroA, Pilha *prisioneiroB){
+	cartaBaralho carta;
+	if (jogadorA->tamanhoPilha() == 0){
+		while(!prisioneiroA->vazia()){
+			carta = prisioneiroA->topo();
+			carta.vira();
+			jogadorA->inserir(carta);
+			prisioneiroA->remover();
+		}
+	}
+	if (jogadorB->tamanhoPilha() == 0){
+		while(!prisioneiroB->vazia()){
+			carta = prisioneiroB->topo();
+			carta.vira();
+			jogadorB->inserir(carta);
+			prisioneiroB->remover();
+		}
 	}
 }
 
-
 int main(){
 	Pilha jogadorA, jogadorB, areaCombate, prisioneiroA, prisioneiroB;
+	int qtdCartasA, qtdCartasB;
 	init(&jogadorA, &jogadorB);
 //	 cout<< "----- A ----- " << jogadorA.tamanhoPilha()<<endl;
 //	 jogadorA.imprime();
 //	 cout<< "----- B ----- " << jogadorB.tamanhoPilha()<<endl;
 //	 jogadorB.imprime();
-
-	cout << "Carta \t Naipe" << endl;
+	cout <<"**************************************"<<endl;
+	cout <<"********     ***GUERRA***     ********"<<endl;
+	cout <<"**************************************"<<endl;
+	cout << "\tCarta\tNaipe" << endl;
+	cout <<"======================================"<<endl;
 	bool game = true;
 	int rodadas = 0;
 	
 	while (game) {
 		cartaBaralho cartaA, cartaB;
+		
 		cartaA = jogadorA.topo();
 		cartaB = jogadorB.topo();
 		//vira as cartas
@@ -78,26 +102,26 @@ int main(){
 		//remove a primeira carta de cada jogador, pois as cartas estao na area de combate
 		jogadorA.remover();
 		jogadorB.remover();
-//		cout << "Cartas - Jogador A: " << prisioneiroA.tamanhoPilha()<<endl;
-//		cout << "Cartas - Jogador B: " << prisioneiroB.tamanhoPilha()<<endl;
 		int valorCartaA = cartaA.valorCarta();
 		int valorCartaB = cartaB.valorCarta();
 		mostraRodada(cartaA, cartaB);
 		comparaCartas(cartaA, cartaB, &areaCombate, &prisioneiroA, &prisioneiroB);
-
+		if (jogadorA.tamanhoPilha() == 0 || jogadorB.tamanhoPilha() == 0){
+			pegaDoPrisioneiro(&jogadorA, &jogadorB, &prisioneiroA, &prisioneiroB);
+		}
 		// Condicoes de parada
-		if (rodadas >= 20) game = false;
-
+		if (rodadas >= 60) game = false;
+		qtdCartasA = prisioneiroA.tamanhoPilha() + jogadorA.tamanhoPilha();
+		qtdCartasB = prisioneiroB.tamanhoPilha() + jogadorB.tamanhoPilha();
 		// Precisa pegar as cartas do prisioneiro
-		if (prisioneiroA.tamanhoPilha() >= 35 || prisioneiroB.tamanhoPilha() >= 35)
+		if ( qtdCartasA >= 35 || qtdCartasB >= 35)
 			game = false;
 
 		rodadas++;
 	}
 
-	cout << "Cartas - Jogador A: " << prisioneiroA.tamanhoPilha()<<endl;
-	cout << "Cartas - Jogador B: " << prisioneiroB.tamanhoPilha()<<endl;
-
+	cout << "Cartas - Jogador A: " << qtdCartasA<<endl;
+	cout << "Cartas - Jogador B: " << qtdCartasB<<endl;
 
 	return 0;
 }

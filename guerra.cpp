99da9dd7475f05f4e384f8674dd4c977ebdc07b5
Pedrofiigueiredo <mnopedrodias@gmail.com>
2020-11-16 14:11:58 -0300

    classe Baralho implementada

--- guerra.cpp
+++ guerra.cpp
@@ -1,104 +1,38 @@
 #include <iostream>
-# include "cartaBaralho.h"
-# include "pilha.h"
-# include <ctime>
+#include <ctime>
+#include "cartaBaralho.h"
+#include "pilha.h"
+#include "utils.h" /* baguncar(), init(), mostraRodada(), comparaCartas(), pegaDoPrisioneiro()*/
 
 using namespace std;
 
-void baguncar(cartaBaralho a[], int tamanho){	
-	srand((unsigned)time(0));
-	for(int i=tamanho-1; i>=1; --i){
-    	swap(a[i], a[rand()%i]);
-	}
-}
-
-void init(Pilha *jogadorA, Pilha *jogadorB){
-	cartaBaralho carta[52];
-
-	int index = 0;
-	for(int i = 1; i < 14; i++){
-		carta[index++] = cartaBaralho(cartaBaralho::Espadas, i);
-		carta[index++] = cartaBaralho(cartaBaralho::Ouros, i);
-		carta[index++] = cartaBaralho(cartaBaralho::Paus, i);
-		carta[index++] = cartaBaralho(cartaBaralho::Copas, i);
-	}
-	baguncar(carta, 52);
-	for(int i = 0; i < 52; i++) { // Distribui as cartas
-		(i % 2 == 0) ? jogadorA->inserir(carta[i]) : jogadorB->inserir(carta[i]);;
-	}
-}
-
-void mostraRodada(cartaBaralho a, cartaBaralho b) {
-	string naipe[] = {"Espadas", "Ouros", "Paus", "Copas"};
-	string c[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	cout <<"\t"<< c[a.valorCarta() - 1] <<"\t"<< naipe[a.valorNaipe()] << endl;
-	cout <<"\t"<< c[b.valorCarta() - 1] <<"\t"<< naipe[b.valorNaipe()] << endl;
-}
-
-void comparaCartas(cartaBaralho a, cartaBaralho b, Pilha *areaCombate, Pilha *prisioneiroA, Pilha *prisioneiroB) {
-	if (a.valorCarta() != b.valorCarta()) {
-		cartaBaralho carta;
-		if (a.valorCarta() > b.valorCarta()){
-			while (!areaCombate->vazia()){
-				prisioneiroA->inserir(areaCombate->topo());
-				areaCombate->remover();
-			}
-		} else {
-			while (!areaCombate->vazia()){
-				prisioneiroB->inserir(areaCombate->topo());
-				areaCombate->remover();
-			}
-		}
-		cout << "--------------------------------------" << endl;
-	}
-}
-void pegaDoPrisioneiro(Pilha *jogadorA, Pilha *jogadorB, Pilha *prisioneiroA, Pilha *prisioneiroB){
-	cartaBaralho carta;
-	if (jogadorA->tamanhoPilha() == 0){
-		while(!prisioneiroA->vazia()){
-			carta = prisioneiroA->topo();
-			carta.vira();
-			jogadorA->inserir(carta);
-			prisioneiroA->remover();
-		}
-	}
-	if (jogadorB->tamanhoPilha() == 0){
-		while(!prisioneiroB->vazia()){
-			carta = prisioneiroB->topo();
-			carta.vira();
-			jogadorB->inserir(carta);
-			prisioneiroB->remover();
-		}
-	}
-}
-
-int main(){
+int main() {
+	Baralho baralho;
 	Pilha jogadorA, jogadorB, areaCombate, prisioneiroA, prisioneiroB;
 	int qtdCartasA, qtdCartasB;
-	init(&jogadorA, &jogadorB);
-//	 cout<< "----- A ----- " << jogadorA.tamanhoPilha()<<endl;
-//	 jogadorA.imprime();
-//	 cout<< "----- B ----- " << jogadorB.tamanhoPilha()<<endl;
-//	 jogadorB.imprime();
+	baralho.init(&jogadorA, &jogadorB);
+
 	cout <<"**************************************"<<endl;
-	cout <<"********     ***GUERRA***     ********"<<endl;
+	cout <<"********        GUERRA        ********"<<endl;
 	cout <<"**************************************"<<endl;
-	cout << "\tCarta\tNaipe" << endl;
-	cout <<"======================================"<<endl;
+
 	bool game = true;
 	int rodadas = 0;
-	
+
 	while (game) {
 		cartaBaralho cartaA, cartaB;
-		
+
 		cartaA = jogadorA.topo();
 		cartaB = jogadorB.topo();
+
 		//vira as cartas
 		cartaA.vira();
 		cartaB.vira();
-		//inserir as cartas na area de combate		
+
+		//inserir as cartas na area de combate
 		areaCombate.inserir(cartaA);
 		areaCombate.inserir(cartaB);
+
 		//remove a primeira carta de cada jogador, pois as cartas estao na area de combate
 		jogadorA.remover();
 		jogadorB.remover();
@@ -109,18 +43,22 @@ int main(){
 		if (jogadorA.tamanhoPilha() == 0 || jogadorB.tamanhoPilha() == 0){
 			pegaDoPrisioneiro(&jogadorA, &jogadorB, &prisioneiroA, &prisioneiroB);
 		}
+
 		// Condicoes de parada
-		if (rodadas >= 60) game = false;
+		if (rodadas >= 60) {
+			game = false;
+			cout << "\nChegamos em 60 rodadas" << endl;
+		};
 		qtdCartasA = prisioneiroA.tamanhoPilha() + jogadorA.tamanhoPilha();
 		qtdCartasB = prisioneiroB.tamanhoPilha() + jogadorB.tamanhoPilha();
-		// Precisa pegar as cartas do prisioneiro
+
 		if ( qtdCartasA >= 35 || qtdCartasB >= 35)
 			game = false;
 
 		rodadas++;
 	}
 
-	cout << "Cartas - Jogador A: " << qtdCartasA<<endl;
+	cout << "\nCartas - Jogador A: " << qtdCartasA<<endl;
 	cout << "Cartas - Jogador B: " << qtdCartasB<<endl;
 
 	return 0;
---
+++ utils.h
@@ -0,0 +1,72 @@
+class Baralho {
+	public:
+		Baralho() {}
+		void baguncar(cartaBaralho a[], int tamanho);
+		void init(Pilha *jogadorA, Pilha *jogadorB);
+};
+
+void Baralho::baguncar(cartaBaralho a[], int tamanho) {
+	srand((unsigned)time(0));
+	for(int i=tamanho-1; i>=1; --i){
+    	swap(a[i], a[rand()%i]);
+	}
+}
+
+void Baralho::init(Pilha *jogadorA, Pilha *jogadorB) { /* Inicializa, embaralha e distribui */
+	cartaBaralho carta[52];
+	int index = 0;
+	for (int i = 1; i < 14; i++) {
+		carta[index++] = cartaBaralho(cartaBaralho::Espadas, i);
+		carta[index++] = cartaBaralho(cartaBaralho::Ouros, i);
+		carta[index++] = cartaBaralho(cartaBaralho::Paus, i);
+		carta[index++] = cartaBaralho(cartaBaralho::Copas, i);
+	}
+	baguncar(carta, 52);
+	for (int i = 0; i < 52; i++) // Distribui as cartas
+		(i % 2 == 0) ? jogadorA->inserir(carta[i]) : jogadorB->inserir(carta[i]);;
+}
+
+void mostraRodada(cartaBaralho a, cartaBaralho b) {
+	string naipe[] = {"Espadas", "Ouros", "Paus", "Copas"};
+	string c[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	cout <<" "<< c[a.valorCarta() - 1] <<"\t"<< naipe[a.valorNaipe()] << endl;
+	cout <<" "<< c[b.valorCarta() - 1] <<"\t"<< naipe[b.valorNaipe()] << endl;
+}
+
+void comparaCartas(cartaBaralho a, cartaBaralho b, Pilha *areaCombate, Pilha *prisioneiroA, Pilha *prisioneiroB) {
+	if (a.valorCarta() != b.valorCarta()) {
+		cartaBaralho carta;
+		if (a.valorCarta() > b.valorCarta()){
+			while (!areaCombate->vazia()){
+				prisioneiroA->inserir(areaCombate->topo());
+				areaCombate->remover();
+			}
+		} else {
+			while (!areaCombate->vazia()){
+				prisioneiroB->inserir(areaCombate->topo());
+				areaCombate->remover();
+			}
+		}
+		cout << "--------------------------------------" << endl;
+	}
+}
+
+void pegaDoPrisioneiro(Pilha *jogadorA, Pilha *jogadorB, Pilha *prisioneiroA, Pilha *prisioneiroB) {
+	cartaBaralho carta;
+	if (jogadorA->tamanhoPilha() == 0){
+		while(!prisioneiroA->vazia()){
+			carta = prisioneiroA->topo();
+			carta.vira();
+			jogadorA->inserir(carta);
+			prisioneiroA->remover();
+		}
+	}
+	if (jogadorB->tamanhoPilha() == 0){
+		while(!prisioneiroB->vazia()){
+			carta = prisioneiroB->topo();
+			carta.vira();
+			jogadorB->inserir(carta);
+			prisioneiroB->remover();
+		}
+	}
+}
